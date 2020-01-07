69090a301d7e3248ae6a9a1000a9aff7d26109ec
sebtedesco <stedescoc@gmail.com>
2020-01-07 01:26:15 +0000

    sgt-react: average grade | Sebastian Tedesco

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -26,14 +26,34 @@ class App extends React.Component {
       });
   }
 
+  gradeAverage() {
+    const studentObject = this.state.grades;
+    const gradesArr = [];
+    let sumOfGrades = 0;
+    let averageGrade = null;
+    for (const index in studentObject) {
+      gradesArr.push(studentObject[index].grade);
+      sumOfGrades += studentObject[index].grade;
+      averageGrade = sumOfGrades / gradesArr.length;
+    }
+    return averageGrade;
+  }
+
   render() {
     return (
-      <div className="container">
-        <div className="row">
-          <Header text="Student Grade Table" />
-          <GradeTable grades={this.state.grades}/>
+      <>
+        <div className="d-flex container w-75 align-items-center">
+          {/* <div className="row w-100 align-items-center"> */}
+          <Header className="col-8" text="Student Grade Table" />
+          <h4 className="col-4 text-right">{`Average Grade: ${this.gradeAverage()}`}</h4>
+          {/* </div> */}
         </div>
-      </div>
+        <div className="container w-75">
+          <div className="row">
+            <GradeTable grades={this.state.grades} />
+          </div>
+        </div>
+      </>
     );
   }
 }
--- client/components/header.jsx
+++ client/components/header.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 function Header(props) {
   return (
-    <h1>{ props.text }</h1>
+    <h2>{ props.text }</h2>
   );
 }
 
